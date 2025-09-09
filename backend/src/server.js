954a19aeb16c7fa30ec0b9284bddf95b2737cfd8
Nelson2023-2024 <nelsonobuya19@gmail.com>
2025-09-09 15:58:49 +0300

    Added the middleware for all the routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,26 @@
 import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
 import { ENV } from "./config/env.config.js";
 import { connectToMongoDB } from "./config/db.config.js";
+import { userRoutes } from "./routes/user.route.js";
+import { notificationRoutes } from "./routes/notification.route.js";
+import { postRoutes } from "./routes/post.route.js";
+import { commentRoutes } from "./routes/comment.route.js";
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+app.use(clerkMiddleware());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+app.use("/api/comment", commentRoutes);
+
 app.listen(ENV.PORT, () => {
-  connectToMongoDB()
+  connectToMongoDB();
   console.log(`Server running on http://localhost:${ENV.PORT}/`);
 });
